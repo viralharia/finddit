03e967530f99e2526cc81803808d3a884fd30be2
Viral Haria <viralhar@gmail.com>
2020-04-02 16:09:15 +0530

    added README.md file and changed the api url to use https

--- redditapi.js
+++ redditapi.js
@@ -1,6 +1,6 @@
 export default {
     search : function(searchTerm, searchLimit, sortBy){
-        return fetch(`http://www.reddit.com/search.json?q=${searchTerm}&sort=${sortBy}&limit=${searchLimit}`)
+        return fetch(`https://www.reddit.com/search.json?q=${searchTerm}&sort=${sortBy}&limit=${searchLimit}`)
                     .then(res => res.json())
                     .then(data => {
                         return data.data.children.map(data => data.data);
